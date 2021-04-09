2abb6fea40de2c92ca5f734d508570aa70e14945
Matthew McCaskill <mattrmccaskill@gmail.com>
2021-04-09 01:16:16 -0500

    Stuff

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,5 +1,4 @@
 import { Client, Message } from 'discord.js';
-import config from '../config/config';
 
 export default function onMessage(client: Client, msg: Message): void {
     // Check if message was in a guild or in a DM
